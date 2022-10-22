375b88364e360ea454e82149dca18dca0ea5dee1
unknown <arfinchowdhuryarif7@gmail.com>
2022-10-22 20:18:48 +0600

    finaly done something

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// for arraw toggle
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+    let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+    console.log(arrowParent)
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+// sidebar functionality
+// let sidebar = document.querySelector(".sidebar");
+// let sidebarBtn = document.querySelector(".bx-menu");
+// console.log(sidebarBtn);
+// sidebarBtn.addEventListener("click", ()=>{
+//   sidebar.classList.toggle("close");
+// });
+
+
+// For Tab Functionality
+
 function openTab(tabName) {
     var i;
     var x = document.getElementsByClassName("tabOptions");
